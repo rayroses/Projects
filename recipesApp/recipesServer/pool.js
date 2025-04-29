da7beed6aac29b9ea0032a8c34fe866fd3e76f12
rayroses <raizyberenbaum@gmail.com>
2025-04-29 01:29:42 -0400

    fixed pool.js

--- recipesApp/recipesServer/pool.js
+++ recipesApp/recipesServer/pool.js
@@ -3,7 +3,7 @@ export default mysql.createPool({
     host: 'database-recipes.c9i6ie4csty0.us-east-2.rds.amazonaws.com',
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
-    database: 'nodeuser1',
+    database: 'database-recipes',
     waitForConnections: true,
     connectionLimit: 10,
     maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
